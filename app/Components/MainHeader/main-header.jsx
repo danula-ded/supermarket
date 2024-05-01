54a6f10e26be7db9a6a72d8d01fce877b91d349d
Stupishin Daniil <stupishin1597@yandex.ru>
2024-05-01 15:48:14 +0300

    save 0.1.2

--- app/Components/MainHeader/main-header.jsx
+++ app/Components/MainHeader/main-header.jsx
@@ -1,13 +1,15 @@
 export const MainHeader= () => {
-<div className="MainHeader" style={{width: 1920, height: 1006, paddingTop: 440, paddingBottom: 187, paddingLeft: 352, paddingRight: 351, justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
-  <div className="HeaderTextButton" style={{alignSelf: 'stretch', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 60, display: 'inline-flex'}}>
-    <div className="HeaderText" style={{width: 1217, height: 219, position: 'relative'}}>
-      <div style={{left: 61, top: 0, position: 'absolute', color: 'black', fontSize: 128, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Шопинг Колледжа</div>
-      <div style={{left: 0, top: 190, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Добро пожаловать в мир вкуса и стиля! У нас есть все, чтобы сделать твою жизнь ярче и вкуснее.</div>
+  return (
+    <div className="MainHeader" style={{width: 1920, height: 1006, paddingTop: 440, paddingBottom: 187, paddingLeft: 352, paddingRight: 351, justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+      <div className="HeaderTextButton" style={{alignSelf: 'stretch', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 60, display: 'inline-flex'}}>
+        <div className="HeaderText" style={{width: 1217, height: 219, position: 'relative'}}>
+          <div style={{left: 61, top: 0, position: 'absolute', color: 'black', fontSize: 128, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Шопинг Колледжа</div>
+          <div style={{left: 0, top: 190, position: 'absolute', color: 'black', fontSize: 24, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Добро пожаловать в мир вкуса и стиля! У нас есть все, чтобы сделать твою жизнь ярче и вкуснее.</div>
+        </div>
+        <div className="Button" style={{paddingLeft: 59, paddingRight: 59, paddingTop: 21, paddingBottom: 21, borderRadius: 90, overflow: 'hidden', border: '2px white solid', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+          <div style={{color: 'white', fontSize: 48, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Погнали!</div>
+        </div>
+      </div>
     </div>
-    <div className="Button" style={{paddingLeft: 59, paddingRight: 59, paddingTop: 21, paddingBottom: 21, borderRadius: 90, overflow: 'hidden', border: '2px white solid', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
-      <div style={{color: 'white', fontSize: 48, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Погнали!</div>
-    </div>
-  </div>
-</div>
+  );
 }
